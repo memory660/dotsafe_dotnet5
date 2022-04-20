9417e6c3f75246f768972a5c70368c7257dbb7fd
duke <webcreation33@yahoo.fr>
2022-04-20 10:21:08 +0200

    first commit

---
+++ libs/Com.Core.Dotsafe.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Core.Dotsafe.Domain
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Array roles { get; set; }
+        public string password { get; set; }
+    }
+}
